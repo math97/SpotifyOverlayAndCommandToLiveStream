3cb257f6afeb189f3e2467c43f409f2e4cc98d83
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2021-08-16 20:22:30 -0300

    config for jsonwebtoken

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -5,5 +5,9 @@ export default {
   clientSecret:process.env.CLIENT_SECRET,
   urlCode:'http://accounts.spotify.com/authorize',
   urlToken:'https://accounts.spotify.com/api/token',
-  scopePlayingCurrentSong:'user-read-currently-playing user-read-playback-state'
+  scopePlayingCurrentSong:'user-read-currently-playing user-read-playback-state',
+  jwt:{
+    secret:process.env.SECRET,
+    expiresIn:'1d',
+  }
 }
