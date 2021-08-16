01ac102b48675c436105f202055a76f24f66b0da
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2021-08-16 18:31:05 -0300

    remove consolelog

--- src/controllers/playlistController.ts
+++ src/controllers/playlistController.ts
@@ -8,21 +8,16 @@ interface artistData{
 export default class PlaylistController { 
   public async getCurrentPlaylist(accessToken:string){
     try {
-
       const getCurrentPlayingTrackService = new GetCurrentPlayingTrackService();
       const dataResponseSpotify = await getCurrentPlayingTrackService.execute(accessToken as string); 
   
       const context = dataResponseSpotify.context;
-
-      console.log(context,'-----context');
     
       if(context === null) return 'this song doesn\'t belong to any playlist';
 
       return context.external_urls.spotify;
       
-    } catch (error) {
-      console.log(error,'----------- controller');
-      
+    } catch (error) {      
       throw new AppError(error.message,error.code);    
     }
   }
