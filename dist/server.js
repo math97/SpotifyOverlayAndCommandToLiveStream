3cb46b3a0bbc0df89a232724792294f7bf61869d
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2021-08-15 21:44:01 -0300

    fix build

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var index_1 = __importDefault(require("./routes/index"));
+var AppError_1 = __importDefault(require("./AppError"));
+var app = express_1.default();
+app.use(express_1.default.json());
+app.use(cors_1.default());
+app.use(index_1.default);
+app.use(function (err, request, response, _) {
+    if (err instanceof AppError_1.default) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
+app.listen(process.env.PORT, function () {
+    console.log("server starts on port " + process.env.PORT);
+});
