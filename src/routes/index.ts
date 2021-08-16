cbb51772e083fb313ac88d33771b8a75690b370c
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2021-08-16 18:08:54 -0300

    create of controller and route to playlist

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,9 @@ import { Request, Response, Router } from 'express';
 
 import authorizationRoutes from './authorization.routes';
 import musicRoutes from './music.routes';
+import playlistRoutes from './playlist.routes';
 
-import GetCurrentPlayingTrackService from '../services/GetCurrentPlayingTrackService';
-import GetCurrentPlaylist from '../services/GetCurrentPlaylist';
 import saveTokensMiddleware from '../middlewares/saveTokens';
-import AppError from '../AppError';
 
 const routes = Router();
 
@@ -16,22 +14,8 @@ routes.get('/',(request,response)=>{
 
 routes.use('/authorization',authorizationRoutes);
 routes.use('/currentPlayingTrack',musicRoutes);
+routes.use('/currentPlaylist',playlistRoutes);
 //routes.use(saveTokensMiddleware);
 
-routes.get('/currentPlaylist',async (request,response)=>{  
-  try {
-    const {accessToken} =  request.query;
-    
-    if(!accessToken || accessToken === undefined) throw new AppError('User not authenticated.Send the correct token',401);
-  
-    const getCurrentPlayingTrackService = new GetCurrentPlaylist();
-    const data = await getCurrentPlayingTrackService.execute(accessToken as string); 
-    
-    return response.json(` ${data} `)
-  } catch (error) {
-    if(error.statusCode === 401) return response.json({Error:"Token not sent or invalid"});
-    throw new AppError(error.message,error.urlCode);
-  }
-});
 
 export default routes;
---
+++ src/routes/playlist.routes.ts
@@ -0,0 +1,24 @@
+import { Router,Request,Response } from 'express';
+
+import PlaylistController from '../controllers/playlistController';
+import AppError from '../AppError';
+
+const playlistRoutes = Router();
+
+playlistRoutes.get('/',async (request,response)=>{  
+  try {
+    const {accessToken} =  request.query;
+    
+    if(!accessToken || accessToken === undefined) throw new AppError('User not authenticated.Send the correct token',401);
+  
+    const playlistController = new PlaylistController();
+    const data = await playlistController.getCurrentPlaylist(accessToken as string); 
+    
+    return response.json(` ${data} `)
+  } catch (error) {
+    if(error.statusCode === 401) return response.json({Error:"Token not sent or invalid"});
+    throw new AppError(error.message,error.urlCode);
+  }
+});
+
+export default playlistRoutes;
